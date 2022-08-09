d2e3ee2b1101a5ecc196424c012c58964ca66753
Lena Araújo <lenaaraujo92@gmail.com>
2022-08-08 22:05:59 -0300

    cards and fonts - wrote down all the cards, chose fonts

---
+++ hiraganamemory.js
@@ -0,0 +1,11 @@
+// Vowels: A I U E O - あ い う え お
+// K: KA KI KU KE KO - か き く け こ
+// S: SA SHI SU SE SO - さ し す せ そ
+// T: TA CHI TSU TE TO - た ち つ て と
+// N: NA NI NU NE NO - な に ぬ ね の
+// H: HA HI FU HE HO - は ひ ふ へ ほ
+// M: MA MI MU ME MO - ま み む め も
+// R: RA RI RU RE RO - ら り る れ ろ
+// Y: YA YU YO - や ゆ よ
+// W: WA WO - わ を
+// N: N - ん
