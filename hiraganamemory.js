ae326597e5e5a0b3334c33050a3cd03d0e0ea70c
Lena Araújo <lenaaraujo92@gmail.com>
2022-08-10 22:00:47 -0300

    objects - refactored 2D arrays into objects

--- hiraganamemory.js
+++ hiraganamemory.js
@@ -10,225 +10,265 @@
 // 9 - W: WA WO - わ を
 // 10 - N: N - ん
 
-const pairsOfCards = [
-  [
-    {
+const cards = {
+  vowelSet: {
+    a: {
       romaji: "A",
       hiragana: "あ",
+      flipped: false,
     },
-    {
+    i: {
       romaji: "I",
       hiragana: "い",
+      flipped: false,
     },
-    {
+    u: {
       romaji: "U",
       hiragana: "う",
+      flipped: false,
     },
-    {
+    e: {
       romaji: "E",
       hiragana: "え",
+      flipped: false,
     },
-    {
+    o: {
       romaji: "O",
       hiragana: "お",
+      flipped: false,
     },
-  ],
-  [
-    {
+  },
+  kSet: {
+    ka: {
       romaji: "KA",
       hiragana: "か",
+      flipped: false,
     },
-    {
+    ki: {
       romaji: "KI",
       hiragana: "き",
+      flipped: false,
     },
-    {
+    ku: {
       romaji: "KU",
       hiragana: "く",
+      flipped: false,
     },
-    {
+    ke: {
       romaji: "KE",
       hiragana: "け",
+      flipped: false,
     },
-    {
+    ko: {
       romaji: "KO",
       hiragana: "こ",
+      flipped: false,
     },
-  ],
-  [
-    {
+  },
+  sSet: {
+    sa: {
       romaji: "SA",
       hiragana: "さ",
+      flipped: false,
     },
-    {
+    shi: {
       romaji: "SHI",
       hiragana: "し",
+      flipped: false,
     },
-    {
+    su: {
       romaji: "SU",
       hiragana: "す",
+      flipped: false,
     },
-    {
+    se: {
       romaji: "SE",
       hiragana: "せ",
+      flipped: false,
     },
-    {
+    so: {
       romaji: "SO",
       hiragana: "そ",
+      flipped: false,
     },
-  ],
-  [
-    {
+  },
+  tSet: {
+    ta: {
       romaji: "TA",
       hiragana: "た",
+      flipped: false,
     },
-    {
+    chi: {
       romaji: "CHI",
       hiragana: "ち",
+      flipped: false,
     },
-    {
+    tsu: {
       romaji: "TSU",
       hiragana: "つ",
+      flipped: false,
     },
-    {
+    te: {
       romaji: "TE",
       hiragana: "て",
+      flipped: false,
     },
-    {
+    to: {
       romaji: "TO",
       hiragana: "と",
+      flipped: false,
     },
-  ],
-  [
-    {
+  },
+  nSet: {
+    na: {
       romaji: "NA",
       hiragana: "な",
+      flipped: false,
     },
-    {
+    ni: {
       romaji: "NI",
       hiragana: "に",
+      flipped: false,
     },
-    {
+    nu: {
       romaji: "NU",
       hiragana: "ぬ",
+      flipped: false,
     },
-    {
+    ne: {
       romaji: "NE",
       hiragana: "ね",
+      flipped: false,
     },
-    {
+    no: {
       romaji: "NO",
       hiragana: "の",
+      flipped: false,
     },
-  ],
-  [
-    {
+  },
+  hSet: {
+    ha: {
       romaji: "HA",
       hiragana: "は",
+      flipped: false,
     },
-    {
+    hi: {
       romaji: "HI",
       hiragana: "ひ",
+      flipped: false,
     },
-    {
+    fu: {
       romaji: "FU",
       hiragana: "ふ",
+      flipped: false,
     },
-    {
+    he: {
       romaji: "HE",
       hiragana: "へ",
+      flipped: false,
     },
-    {
+    ho: {
       romaji: "HO",
       hiragana: "ほ",
+      flipped: false,
     },
-  ],
-  [
-    {
+  },
+  mSet: {
+    ma: {
       romaji: "MA",
       hiragana: "ま",
+      flipped: false,
     },
-    {
+    mi: {
       romaji: "MI",
       hiragana: "み",
+      flipped: false,
     },
-    {
+    mu: {
       romaji: "MU",
       hiragana: "む",
+      flipped: false,
     },
-    {
+    me: {
       romaji: "ME",
       hiragana: "め",
+      flipped: false,
     },
-    {
+    mo: {
       romaji: "MO",
       hiragana: "も",
+      flipped: false,
     },
-  ],
-  [
-    {
+  },
+  rSet: {
+    ra: {
       romaji: "RA",
       hiragana: "ら",
+      flipped: false,
     },
-    {
+    ri: {
       romaji: "RI",
       hiragana: "り",
+      flipped: false,
     },
-    {
+    ru: {
       romaji: "RU",
       hiragana: "る",
+      flipped: false,
     },
-    {
+    re: {
       romaji: "RE",
       hiragana: "れ",
+      flipped: false,
     },
-    {
+    ro: {
       romaji: "RO",
       hiragana: "ろ",
+      flipped: false,
     },
-  ],
-  [
-    {
+  },
+  ySet: {
+    ya: {
       romaji: "YA",
       hiragana: "や",
+      flipped: false,
     },
-    {
+    yu: {
       romaji: "YU",
       hiragana: "ゆ",
+      flipped: false,
     },
-    {
+    yo: {
       romaji: "YO",
       hiragana: "よ",
+      flipped: false,
     },
-  ],
-  [
-    {
+  },
+  wSet: {
+    wa: {
       romaji: "WA",
       hiragana: "わ",
+      flipped: false,
     },
-    {
+    wo: {
       romaji: "WO",
       hiragana: "を",
+      flipped: false,
     },
-  ],
-  [
-    {
+  },
+  nPhoneme: {
+    n: {
       romaji: "N",
       hiragana: "ん",
+      flipped: false,
     },
-  ],
-];
+  },
+};
 
-const vowels = pairsOfCards[0];
-const k = pairsOfCards[1];
-const s = pairsOfCards[2];
-const t = pairsOfCards[3];
-const n = pairsOfCards[4];
-const h = pairsOfCards[5];
-const m = pairsOfCards[6];
-const r = pairsOfCards[7];
-const y = pairsOfCards[8];
-const w = pairsOfCards[9];
-const n = pairsOfCards[10];
+// Select sets of cards
 
-const table = document.querySelector("#board");
+// Shuffle cards
+
+// Check for a pair & update counter
+
+// End game
